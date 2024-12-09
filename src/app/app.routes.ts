e42cde5ba70fe9149ccbc1e2af3c79d29eaa7349
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-12-09 18:18:30 +0100

    feature(event): can navigate to event details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import {EventListComponent} from './event-list/event-list.component';
+import {EventComponent} from './event/event.component';
 
 export const routes: Routes = [
   {
     path: "events",
     component: EventListComponent
+  },
+  {
+    path: "events/:id",
+    component: EventComponent
   }
 ];
---
+++ src/app/event/event.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event',
+  imports: [],
+  templateUrl: './event.component.html',
+  standalone: true,
+  styleUrl: './event.component.scss'
+})
+export class EventComponent {
+
+}
