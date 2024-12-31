3d473c334129d9bf65489cd2cb963cf43c2c371a
zounkla <flo-vl@orange.fr>
2024-12-31 11:21:58 +0100

    feat: add success popup when deleting an event

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -3,6 +3,7 @@ import {Event} from '../models/event.model';
 import {EventService} from '../services/event.service';
 import {Router} from '@angular/router';
 import {NgOptimizedImage} from '@angular/common';
+import {PopupService} from '../services/popup.service';
 
 @Component({
   selector: 'app-event',
@@ -18,15 +19,18 @@ export class EventComponent implements OnInit {
   @Input() id: string = "";
   event: Event | null = null;
 
-  constructor(private service: EventService, private router: Router){}
+  constructor(private service: EventService, private router: Router,
+              private popupService: PopupService){}
 
   ngOnInit(): void {
     this.service.getEvent(this.id).subscribe(data => this.event = data);
   }
 
   deleteEvent(): void {
-    this.service.deleteEvent(this.id).subscribe(
-      () => this.router.navigate(["/events"])
+    this.service.deleteEvent(this.id).subscribe(() => {
+        this.popupService.openSuccess('Event deleted successfully.');
+        this.router.navigate(["/events"])
+      }
     );
   }
 }
